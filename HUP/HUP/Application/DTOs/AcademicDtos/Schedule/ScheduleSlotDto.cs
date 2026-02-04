42b76a5266d9d0a5c902c8ab9575cb1a335ba4de
Menna Magdy <menna11hassanien@gmail.com>
2026-02-05 01:26:26 +0200

    .

--- HUP/HUP/Application/DTOs/AcademicDtos/Schedule/ScheduleSlotDto.cs
+++ HUP/HUP/Application/DTOs/AcademicDtos/Schedule/ScheduleSlotDto.cs
@@ -2,9 +2,12 @@ namespace HUP.Application.DTOs.AcademicDtos.Schedule;
 
 public class ScheduleSlotDto
 {
-    public Guid CourseOfferingId { get; set; }
-    public DayOfWeek DayOfWeek { get; set; }
-    public DateTime StartTime { get; set; }
-    public DateTime EndTime { get; set; }
+    public string CourseName { get; set; }
+    public string CourseCode { get; set; }
+    public string DayOfWeek { get; set; }
+    public TimeSpan StartTime { get; set; }
+    public TimeSpan EndTime { get; set; }
+    public string InstructorName { get; set; }
+    public string Hall { get; set; }
     public string Group { get; set; }
 }
